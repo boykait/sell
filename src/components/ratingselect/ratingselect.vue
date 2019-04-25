ccbe343ce2bbfeb511717d85a6ff62b643294980
boykait <gxw255613@126.com>
2019-04-25 08:19:42 +0800

    评价选择组件

--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,11 +1,24 @@
 <template>
   <div class="ratingselect">
-    <div class="rating-type">
-      <span>{{desc.all}}</span>
-      <span>{{desc.positive}}</span>
-      <span>{{desc.negative}}</span>
+    <div class="rating-type border-1px">
+      <span class="block positive" :class="{'active': selectType === 2}"
+      @click="select(2)">
+        {{desc.all}}
+        <span class="count">{{ratings.length}}</span>
+      </span>
+      <span class="block positive" :class="{'active': selectType === 0}"
+            @click="select(0)">
+        {{desc.positive}}
+        <span class="count">{{positives.length}}</span>
+      </span>
+      <span class="block negative" :class="{'active': selectType === 1}"
+            @click="select(1)">
+        {{desc.negative}}
+        <span class="count">{{negatives.length}}</span>
+      </span>
     </div>
-    <div class="switch">
+    <div class="switch" :class="{'on': onlyContent}"
+      @click="toggleContent">
       <span class="icon-check_circle">
       </span>
       <span class="text">只看有内容的评价</span>
@@ -42,10 +55,88 @@ export default {
         }
       )
     }
+  },
+  methods: {
+    select(type) {
+      this.selectType = type;
+      this.$emit('ratingType-select', type);
+    },
+    toggleContent() {
+      this.onlyContent = !this.onlyContent;
+      this.$emit('ratingType-toggleContent', type);
+    }
+  },
+  computed: {
+    positives() {
+      return this.ratings.filter(r => r.rateType === POSITIVE);
+    },
+    negatives() {
+      return this.ratings.filter(r => r.rateType === NEGATIVE);
+    }
   }
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+  @import "../../common/stylus/mixin";
+  .ratingselect {
+    .rating-type {
+      padding: 18px 0;
+      margin: 0 18px;
+      @include border-1px(rgba(9, 17, 27, 0.1));
+      .block {
+        display: inline-block;
+        padding: 8px 12px;
+        margin-right: 8px;
+        line-height: 16px;
+        border-radius: 1px;
+        font-size: 12px;
+        color: rgb(77, 85, 93);
+        &.active {
+          color: #fff;
+        }
+        &.positive {
+          background-color: rgba(0 ,160, 220, 0.2);
+          &.active {
+            background: rgb(0, 160, 220);
+          }
+        }
+        &.negative {
+          background-color: rgba(77, 85, 93, 0.2);
+          &.active {
+            background: rgb(77, 85, 93);
+          }
+        }
+        .count {
+          margin-left: 2px;
+          font-size: 8px;
+        }
+      }
+    }
+    .switch {
+      padding: 12px 18px;
+      line-height: 24px;
+      border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+      font-size: 0;
+      color: rgb(147, 153, 159);
+      .icon-check_circle {
+        margin-right: 4px;
+        font-size: 24px;
+      }
+      .text {
+        display: inline-block;
+        vertical-align: top;
+        font-size: 12px;
+      }
+      &.on {
+        .icon-check_circle {
+          color: #00c850;
+        }
+      }
+      &.off {
 
+      }
+    }
+
+  }
 </style>
